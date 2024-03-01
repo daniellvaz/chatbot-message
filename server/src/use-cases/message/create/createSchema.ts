0cf0d8937d8c294477a32f2632bf0d0957ec4994
daniellvaz <daniellmvaz@gmail.com>
2024-03-01 13:36:20 -0300

    feat: create message

--- server/src/use-cases/message/create/createSchema.ts
+++ server/src/use-cases/message/create/createSchema.ts
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 export const messageCreateSchema = z.object({
   content: z.string({ required_error: "O campo conteúdo é obrigatório" }),
-  userId: z
+  room: z
     .string({ required_error: "Usuário não autorizado" })
     .cuid("Usuário não autorizado"),
 });
