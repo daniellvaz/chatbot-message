f9e21f3ce3f412e0fb33035253a9fffcd63b1bea
daniellvaz <daniellmvaz@gmail.com>
2024-03-01 13:35:57 -0300

    feat: create user route

--- server/src/routes/api.routes.ts
+++ server/src/routes/api.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { userRegisterController } from "../use-cases/user/register";
 import { messageFindController } from "../use-cases/message/find";
 import ensureAuthenticated from "../middlewares/ensure-authenticated";
+import { userEmailVeriricationController } from "../use-cases/user/email-verification";
 
 export const apiRouter = Router();
 
@@ -9,4 +10,7 @@ apiRouter
   .get("/messages", ensureAuthenticated, (req, res) =>
     messageFindController.execute(req, res)
   )
-  .post("/user", (req, res) => userRegisterController.execute(req, res));
+  .post("/user", (req, res) => userRegisterController.execute(req, res))
+  .get("/user/verify/:id", (req, res) =>
+    userEmailVeriricationController.execute(req, res)
+  );
