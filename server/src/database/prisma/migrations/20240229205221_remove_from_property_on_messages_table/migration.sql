6825164098e28cf1860ec6b3fff8d9e66dc5997a
daniellvaz <daniellmvaz@gmail.com>
2024-03-01 14:00:24 -0300

    refactore: delete migrations

--- server/src/database/prisma/migrations/20240229205221_remove_from_property_on_messages_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `from` on the `messages` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "messages" DROP COLUMN "from";
--- server/src/database/prisma/migrations/20240301012205_update_messages_tabel_remove_attendance_property/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `attendant_id` on the `messages` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "messages" DROP COLUMN "attendant_id";
