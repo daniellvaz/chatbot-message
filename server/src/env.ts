a4bc036f7644618c822f8787633121bb277253e8
daniellvaz <daniellmvaz@gmail.com>
2024-03-01 08:30:48 -0300

    chore: add .env.example file

--- server/src/env.ts
+++ server/src/env.ts
@@ -2,7 +2,15 @@ import * as z from "zod";
 
 export const envSchema = z.object({
   PORT: z.string(),
+  NODE_ENV: z.string(),
+  APP_URL: z.string(),
   API_SECRET_KEY: z.string(),
+  DATABASE_USER: z.string(),
+  DATABASE_PASSWORD: z.string(),
+  DATABASE_HOST: z.string(),
+  DATABASE_PORT: z.string(),
+  DATABASE_NAME: z.string(),
+  DATABASE_URL: z.string(),
   EMAIL_HOST: z.string(),
   EMAIL_PORT: z.string(),
   EMAIL_USER: z.string(),
